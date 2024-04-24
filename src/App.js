42bee9f0b3f4c31d0c0191d7961f677ce65fd845
Minha Lee <shjune1994@gmail.com>
2024-04-24 13:43:45 -0400

    Set up cors setting and connection between client and server side

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
 function App() {
+  fetch("http://localhost:4000/api/data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ key: "testing" }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Server response:", data);
+    })
+    .catch((error) => {
+      console.error("There was a problem with your fetch operation:", error);
+    });
+
   return <div className="App">{process.env.REACT_APP_DB_SERVER}</div>;
 }
 
--- src/server/server.js
+++ src/server/server.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const { sequelize, sellThruVendor } = require("./config/dbConnection");
 const PORT = process.env.PORT || 4000;
-const db = require("./config/dbConnection").sequelize;
 const dbConfig = require("./config/dbConfig").test;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
 
 sequelize.sync();
 
@@ -22,6 +28,11 @@ app.get("/get/data", (req, res) => {
     .catch((err) => {
       throw err;
     });
+});
+
+app.post("/api/data", (req, res) => {
+  const requestData = req.body;
+  res.json({ message: "Data received successfully!", data: requestData });
 });
 
 app.listen(PORT, () => {
